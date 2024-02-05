e12ae8dd2e01df03ca6c94ba85062cc69b400855
DGHDidi <105062809+DGHDidi@users.noreply.github.com>
2024-02-05 15:58:25 +0800

    Create SegmentTree.cpp

---
+++ SegmentTree.cpp
@@ -0,0 +1,100 @@
+template<class Info>
+struct SegmentTree {
+    int n;
+    vector<Info> tree;
+    SegmentTree() : n(0) {}
+    SegmentTree(int _n, Info _v = Info()) {
+        init(_n, _v);
+    }
+    template<class T>
+    SegmentTree(vector<T> _init) {
+        init(_init);
+    }
+    void init(int _n, Info _v = Info()) {
+        init(vector<Info>(_n, _v));
+    }
+    template<class T>
+    void init(vector<T> _init) {
+        n = _init.size();
+        tree.assign((n << 2) + 5, Info());
+        function<void(int, int, int)> build = [&](int now, int l, int r) -> void {
+            if (l == r) {
+                tree[now] = _init[l];
+                return;
+            }
+            int mid = (l + r) >> 1;
+            build(now << 1, l, mid);
+            build(now << 1 | 1, mid + 1, r);
+            pull(now);
+            };
+        build(1, 0, n);
+    }
+    void pull(int now) {
+        tree[now] = tree[now << 1] + tree[now << 1 | 1];
+    }
+    void modify(int now, int l, int r, int pos, const Info &v) {
+        if (l == r) {
+            tree[now] = v;
+            return;
+        }
+        int mid = (l + r) >> 1;
+        if (pos <= mid) {
+            modify(now << 1, l, mid, pos, v);
+        } else {
+            modify(now << 1 | 1, mid + 1, r, pos, v);
+        }
+        pull(now);
+    }
+    void modify(int pos, const Info &v) {
+        modify(1, 0, n, pos, v);
+    }
+    Info rangeQuery(int now, int l, int r, int x, int y) {
+        if (x <= l && r <= y) {
+            return tree[now];
+        }
+        int mid = (l + r) >> 1;
+        Info res = Info();
+        if (x <= mid) {
+            res = res + rangeQuery(now << 1, l, mid, x, y);
+        }
+        if (y > mid) {
+            res = res + rangeQuery(now << 1 | 1, mid + 1, r, x, y);
+        }
+        return res;
+    }
+    Info rangeQuery(int l, int r) {
+        return rangeQuery(1, 0, n, l, r);
+    }
+    template<class F>
+    int findFirst(int now, int l, int r, int x, int y, F check) {
+        if (l == r) {
+            return check(tree[now]) ? l : -1;
+        }
+        int mid = (l + r) >> 1;
+        int res = findFirst(now << 1, l, mid, x, y, check);
+        if (res == -1) {
+            res = findFirst(now << 1 | 1, mid + 1, r, x, y, check);
+        }
+        return res;
+    }
+    template<class F>
+    int findFirst(int l, int r, F check) {
+        return findFirst(1, 0, n, l, r, check);
+    }
+    template<class F>
+    int findLast(int now, int l, int r, int x, int y, F check) {
+        if (l == r) {
+            return check(tree[now]) ? l : -1;
+        }
+        int mid = (l + r) >> 1;
+        int res = findLast(now << 1 | 1, mid + 1, r, x, y, check);
+        if (res == -1) {
+            res = findLast(now << 1, l, mid, x, y, check);
+        }
+        return res;
+    }
+    template<class F>
+    int findLast(int l, int r, F check) {
+        return findLast(1, 0, n, l, r, check);
+    }
+};
