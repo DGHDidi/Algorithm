f3c53e1deb7bb2616200647cbf15ca55c07262aa
DGHDidi <105062809+DGHDidi@users.noreply.github.com>
2024-02-20 10:58:54 +0800

    Create Disjoint Set Union.cpp

---
+++ Disjoint Set Union.cpp
@@ -0,0 +1,19 @@
+struct DSU {
+    vector<int> fa, siz;
+    DSU(int n) : fa(n + 1), siz(n + 1, 1) {
+        iota(fa.begin(), fa.end(), 0);
+    }
+    int find(int x) {
+        return x == fa[x] ? x : fa[x] = find(fa[x]);
+    }
+    bool merge(int x, int y) {
+        x = find(x), y = find(y);
+        if (x == y) {
+            return false;
+        }
+        fa[x] = y, siz[y] += siz[x];
+    }
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+};
