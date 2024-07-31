b8e06dadbb4609704f919efe674cc7af001421b5
DGHDidi <105062809+DGHDidi@users.noreply.github.com>
2024-07-31 16:26:09 +0800

    Create SCC.cpp

---
+++ SCC.cpp
@@ -0,0 +1,47 @@
+struct SCC {
+    int n, idx = 0, scc_cnt = 0;
+    vector<vector<int>> G;
+
+    SCC(int _n) : n(_n + 1), G(_n + 1) {}
+
+    void add_edge(int u, int v) {
+        G[u].push_back(v);
+    }
+
+    vector<vector<int>> scc() {
+        vector<int> ord(n, -1), low(n), scc_id(n), on_stack(n);;
+        stack<int> stk;
+        vector<vector<int>> scc;
+        function<void(int)> dfs = [&](int u) -> void {
+            ord[u] = low[u] = idx++;
+            stk.push(u), on_stack[u] = true;
+            for (int v : G[u]) {
+                if (ord[v] == -1) {
+                    dfs(v);
+                    low[u] = min(low[u], low[v]);
+                } else if (on_stack[v]) {
+                    low[u] = min(low[u], ord[v]);
+                }
+            }
+            if (ord[u] == low[u]) {
+                scc.push_back({});
+                while (true) {
+                    int v = stk.top();
+                    stk.pop(), on_stack[v] = false;
+                    scc_id[v] = scc_cnt;
+                    scc.back().push_back(v);
+                    if (v == u) {
+                        break;
+                    }
+                }
+                scc_cnt++;
+            }
+            };
+        for (int i = 1; i < n; i++) {
+            if (ord[i] == -1) {
+                dfs(i);
+            }
+        }
+        return scc;
+    }
+};
